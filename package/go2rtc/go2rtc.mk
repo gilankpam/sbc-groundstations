e2772099201853c1084405a90589d2a4aaad45e8
Gilang <gilankpam@gmail.com>
2026-01-04 21:13:07 +0700

    add go2rtc

---
+++ package/go2rtc/go2rtc.mk
@@ -0,0 +1,17 @@
+###############################################################################
+#
+# GO2RTC
+#
+###############################################################################
+
+GO2RTC_VERSION = v1.9.3
+GO2RTC_SOURCE = go2rtc_linux_arm64
+GO2RTC_SITE = https://github.com/AlexxIT/go2rtc/releases/download/$(GO2RTC_VERSION)
+GO2RTC_SITE_METHOD = wget
+GO2RTC_INSTALL_TARGET = YES
+
+define GO2RTC_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 $(@D)/go2rtc_linux_arm64 $(TARGET_DIR)/usr/bin/go2rtc
+	$(INSTALL) -m 755 -D $(GO2RTC_PKGDIR)/files/S99go2rtc $(TARGET_DIR)/etc/init.d/S99go2rtc
+endef
+$(eval $(generic-package))
