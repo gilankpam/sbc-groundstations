d31fb8bf824556fd95cdead585956cd2022d337c
Gilang <gilankpam@gmail.com>
2026-01-05 09:06:03 +0700

    fix go2rtc

--- package/go2rtc/go2rtc.mk
+++ package/go2rtc/go2rtc.mk
@@ -4,11 +4,13 @@
 #
 ###############################################################################
 
-GO2RTC_VERSION = v1.9.3
+GO2RTC_VERSION = v1.9.13
 GO2RTC_SOURCE = go2rtc_linux_arm64
 GO2RTC_SITE = https://github.com/AlexxIT/go2rtc/releases/download/$(GO2RTC_VERSION)
 GO2RTC_SITE_METHOD = wget
 GO2RTC_INSTALL_TARGET = YES
+
+GO2RTC_EXTRACT_CMDS =
 
 define GO2RTC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 $(@D)/go2rtc_linux_arm64 $(TARGET_DIR)/usr/bin/go2rtc
