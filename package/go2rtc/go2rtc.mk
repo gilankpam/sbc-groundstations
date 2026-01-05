83c93d292b69f07033f688829599528c86285e33
Gilang <gilankpam@gmail.com>
2026-01-05 12:29:14 +0700

    fix go2rtc

--- package/go2rtc/go2rtc.mk
+++ package/go2rtc/go2rtc.mk
@@ -7,13 +7,16 @@
 GO2RTC_VERSION = v1.9.13
 GO2RTC_SOURCE = go2rtc_linux_arm64
 GO2RTC_SITE = https://github.com/AlexxIT/go2rtc/releases/download/$(GO2RTC_VERSION)
-GO2RTC_SITE_METHOD = wget
 GO2RTC_INSTALL_TARGET = YES
 
-GO2RTC_EXTRACT_CMDS =
+define GO2RTC_EXTRACT_CMDS
+	cp $(GO2RTC_DL_DIR)/$(GO2RTC_SOURCE) $(@D)
+	chmod 755 $(@D)/$(GO2RTC_SOURCE)
+endef
 
 define GO2RTC_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 $(@D)/go2rtc_linux_arm64 $(TARGET_DIR)/usr/bin/go2rtc
+	$(INSTALL) -m 755 -D $(@D)/$(GO2RTC_SOURCE) $(TARGET_DIR)/usr/bin/go2rtc
 	$(INSTALL) -m 755 -D $(GO2RTC_PKGDIR)/files/S99go2rtc $(TARGET_DIR)/etc/init.d/S99go2rtc
 endef
+
 $(eval $(generic-package))
