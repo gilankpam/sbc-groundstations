a9d87dc9791f66bd609d280cae154b946f9da4c3
Gilang <gilankpam@gmail.com>
2026-01-05 14:19:35 +0700

    fix gstreamer-tee

--- package/gstreamer-tee/gstreamer-tee.mk
+++ package/gstreamer-tee/gstreamer-tee.mk
@@ -7,6 +7,6 @@
 GSTREAMER_TEE_INSTALL_TARGET = YES
 
 define GSTREAMER_TEE_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 -D $(GSTREAMER_TEE_PKGDIR)/files/S97gstreamer-tee $(TARGET_DIR)/etc/init.d/S97gstreamer-tee
+	$(INSTALL) -m 755 -D $(GSTREAMER_TEE_PKGDIR)/files/S99gstreamer-tee $(TARGET_DIR)/etc/init.d/S99gstreamer-tee
 endef
 $(eval $(generic-package))
